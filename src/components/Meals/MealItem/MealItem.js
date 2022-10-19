377b2dc580fa9d5b9d3f9427d2e2ea8465e98554
mshoaibtalha <mshoaibtalha@gmail.com>
2022-10-19 15:54:22 +0200

    change in css

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 
 import MealItemForm from './MealItemForm';
-import classes from './MealItem.module.css';
+import './MealItem.css';
 import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
@@ -9,21 +9,21 @@ const MealItem = (props) => {
 
   const price = `$${props.price.toFixed(2)}`;
 
-  const addToCartHandler = amount => {
+  const addToCartHandler = (amount) => {
     cartCtx.addItem({
       id: props.id,
       name: props.name,
       amount: amount,
-      price: props.price
+      price: props.price,
     });
   };
 
   return (
-    <li className={classes.meal}>
+    <li className='meal'>
       <div>
         <h3>{props.name}</h3>
-        <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
+        <div className='description'>{props.description}</div>
+        <div className='price'>{price}</div>
       </div>
       <div>
         <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
