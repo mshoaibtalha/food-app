182dae7c934901b636c8f3cc26e8990e7cd2f6eb
mshoaibtalha <mshoaibtalha@gmail.com>
2022-10-24 09:56:17 +0200

    cart item css

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,4 +1,4 @@
-import classes from './CartItem.module.css';
+import './CartItem.css';
 
 const CartItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -7,12 +7,12 @@ const CartItem = (props) => {
     <li className={classes['cart-item']}>
       <div>
         <h2>{props.name}</h2>
-        <div className={classes.summary}>
-          <span className={classes.price}>{price}</span>
-          <span className={classes.amount}>x {props.amount}</span>
+        <div className='summary'>
+          <span className='price'>{price}</span>
+          <span className='amount'>x {props.amount}</span>
         </div>
       </div>
-      <div className={classes.actions}>
+      <div className='actions'>
         <button onClick={props.onRemove}>−</button>
         <button onClick={props.onAdd}>+</button>
       </div>
